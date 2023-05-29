2983c12c55f5a582803c26b2585bf4655d72da47
falzzze <light.falze@gmail.com>
2023-05-29 04:01:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Costs from './components/Costs/Costs';
 
 function App() {
+
+  const costs = [
+    {
+      date: new Date(2022, 2, 12),
+      description: 'Холодильник',
+      amount: 999.99,
+    },
+    {
+      date: new Date(2022, 11, 25),
+      description: 'MacBook',
+      amount: 1254.99,
+    },
+    {
+      date: new Date(2022, 4, 1),
+      description: 'Джинсы',
+      amount: 49.99,
+    }
+  ]
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Starting!</h1>
+      <Costs costs={costs}/>
     </div>
   );
 }
+
+
 
 export default App;
